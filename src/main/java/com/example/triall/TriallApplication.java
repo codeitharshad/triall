fa3029773fb29e09c640322c875bf4db3d20218f
Harshad Gangurde <143880663+codeitharshad@users.noreply.github.com>
2026-03-23 21:23:14 +0530

    trial

---
+++ src/main/java/com/example/triall/TriallApplication.java
@@ -0,0 +1,15 @@
+package com.example.triall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriallApplication.class, args);
+		System.out.println("Application is RUNNING");
+	}
+
+
+}
---
+++ src/test/java/com/example/triall/TriallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.triall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
