c37be22c63f339b6668b7df0e9074716f520cee1
Harshad Gangurde <143880663+codeitharshad@users.noreply.github.com>
2026-03-23 22:20:03 +0530

    fixed jar to war

--- src/main/java/com/example/triall/TriallApplication.java
+++ src/main/java/com/example/triall/TriallApplication.java
@@ -2,14 +2,22 @@ package com.example.triall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TriallApplication {
+public class TriallApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TriallApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TriallApplication.class, args);
 		System.out.println("Application is RUNNING");
 	}
+
 
 
 }
